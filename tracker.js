c4ec38f3258cd0c0a425532a15b7c044089d784d
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-02-18 09:58:03 -0700

    initial commit, starting the connection with mysql

---
+++ tracker.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQL_PASSWORD
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+})
